25fa036b78309bd897dcae2fd41a20ca4279d684
ibuki1234 <ibuki.okabe@gmail.com>
2020-06-28 15:20:02 +0900

    test1

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,8 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == 'おい槇原':
+    
+if message.content == 'おい槇原':
         await message.channel.send('もうシャブなんてしないなんて言わないよ絶対')  
         await message.channel.send('https://nikkan-spa.jp/wp-content/uploads/2017/12/yakubutsu2017-550x357.jpg')
 
