f794590dee6854001ee611dfb38b043dbe46fbe0
ibuki1234 <ibuki.okabe@gmail.com>
2020-07-05 13:59:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,15 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == 'おい槇原':
+        await message.channel.send('もうシャブなんてしないなんて言わないよ絶対')  
+        await message.channel.send('https://nikkan-spa.jp/wp-content/uploads/2017/12/yakubutsu2017-550x357.jpg')
+
 
 bot.run(token)
